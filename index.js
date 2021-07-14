44acedf4dfe90815ae71676416b18ba09a458f6e
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-07-14 11:01:00 +0530

    refactor: no changes
    Used Prettier to make the code look much cleaner.

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-/* Before running this program, run the following commands in terminal to install dependencies.
-1. npm install readline-sync
-2. npm install chalk
-*/
-let readlinesync = require('readline-sync');
-let chalk = require('chalk');
+let readlinesync = require("readline-sync");
+let chalk = require("chalk");
 
-console.log(chalk.green("\nWELCOME TO QUIZ GAME\n\nCOLORS TRIVIA") + chalk.bgRed("\nRules") + (":\n1. You score 1 point if you answer correct.\n2. You loose 1 point if you answer wrong.\nIt's as simple as that 🙃 \n"));
+console.log(
+  chalk.green("\nWELCOME TO QUIZ GAME\n\nCOLORS TRIVIA") +
+    chalk.bgRed("\nRules") +
+    ":\n1. You score 1 point if you answer correct.\n2. You loose 1 point if you answer wrong.\nIt's as simple as that 🙃 \n"
+);
 
 let userName = readlinesync.question("So, Whats's your name?\n");
 let score = 0;
 
 console.log("\nWelcome " + chalk.bold.green(userName));
 
-console.log(chalk.underline("\nBefore starting the game,\nLet me verify whether you are a human or not 😆.\n\n"));
+console.log(
+  chalk.underline(
+    "\nBefore starting the game,\nLet me verify whether you are a human or not 😆.\n\n"
+  )
+);
 
-let userVerifyAnswer = readlinesync.question("Calculate the below mathematical expression\n(25*(100+5)/5+125-50) = ");
+let userVerifyAnswer = readlinesync.question(
+  "Calculate the below mathematical expression\n(25*(100+5)/5+125-50) = "
+);
 //Use BODMAS to solve the above expression
 
 function ask(question, answer) {
@@ -29,32 +35,40 @@ function ask(question, answer) {
   console.log("Your current score is ", score);
 }
 
-if (userVerifyAnswer === '600') {
-  console.log(chalk.green("Right Answer !") + ("\nYou are a human.\n\nLet's play ") + chalk.black.bgGreen("Level 1"));
+if (userVerifyAnswer === "600") {
+  console.log(
+    chalk.green("Right Answer !") +
+      "\nYou are a human.\n\nLet's play " +
+      chalk.black.bgGreen("Level 1")
+  );
 
   //Level-One
 
   let levelOneQuestions = [
     {
-      question: '\nWhich planet is called the Red Planet?\n',
-      answer: 'Mars'
+      question: "\nWhich planet is called the Red Planet?\n",
+      answer: "Mars",
     },
     {
-      question: '\nWhat pigment is present in all green plants that give them their color?\n',
-      answer: 'Chlorophyll'
+      question:
+        "\nWhat pigment is present in all green plants that give them their color?\n",
+      answer: "Chlorophyll",
     },
     {
-      question: '\nWhat is the color of the letters O and E in the logo of the search engine Google?\n',
-      answer: 'Red'
+      question:
+        "\nWhat is the color of the letters O and E in the logo of the search engine Google?\n",
+      answer: "Red",
     },
     {
-      question: '\nHow many squares of alternating colors are there on a chessboard?\n',
-      answer: '64'
+      question:
+        "\nHow many squares of alternating colors are there on a chessboard?\n",
+      answer: "64",
     },
     {
-      question: '\nWhich hair color is the rarest naturally-occurring in hair?\n',
-      answer: 'red'
-    }
+      question:
+        "\nWhich hair color is the rarest naturally-occurring in hair?\n",
+      answer: "red",
+    },
   ];
 
   for (let i = 0; i < levelOneQuestions.length; i++) {
@@ -66,29 +80,34 @@ if (userVerifyAnswer === '600') {
 
   let levelTwoQuestions = [
     {
-      question: '\nWhich reptile has the ability to change its color?\n',
-      answer: 'Chameleon'
+      question: "\nWhich reptile has the ability to change its color?\n",
+      answer: "Chameleon",
     },
     {
-      question: '\nWhat is the first color a baby sees?\n',
-      answer: 'Red'
+      question: "\nWhat is the first color a baby sees?\n",
+      answer: "Red",
     },
     {
-      question: '\nWhat is the color to which mosquitoes are most attracted to?\n',
-      answer: 'Blue'
+      question:
+        "\nWhat is the color to which mosquitoes are most attracted to?\n",
+      answer: "Blue",
     },
     {
-      question: '\nWhich color in food stimulate hunger?\n',
-      answer: 'Red'
+      question: "\nWhich color in food stimulate hunger?\n",
+      answer: "Red",
     },
     {
-      question: '\nWhich color is the safest color to prevent road accidents?\n',
-      answer: 'white'
-    }
+      question:
+        "\nWhich color is the safest color to prevent road accidents?\n",
+      answer: "white",
+    },
   ];
 
   if (score === 5) {
-    console.log(("\nYou are doing well. Let me ask some more...\n") + chalk.black.bgYellow("\nLevel Two\n"));
+    console.log(
+      "\nYou are doing well. Let me ask some more...\n" +
+        chalk.black.bgYellow("\nLevel Two\n")
+    );
 
     for (let i = 0; i < levelTwoQuestions.length; i++) {
       let currentQuestion = levelTwoQuestions[i];
@@ -100,56 +119,75 @@ if (userVerifyAnswer === '600') {
 
   let levelThreeQuestions = [
     {
-      question: '\nWhat is the name of the color invented which the human eye cannot see ?\nHINT : They have used this on a BMW car.\n',
-      answer: 'vantablack'
+      question:
+        "\nWhat is the name of the color invented which the human eye cannot see ?\nHINT : They have used this on a BMW car.\n",
+      answer: "vantablack",
     },
     {
-      question: "\nWhat color is used in the logo of the popular kid's channel Nickelodeon?\n",
-      answer: 'orange'
+      question:
+        "\nWhat color is used in the logo of the popular kid's channel Nickelodeon?\n",
+      answer: "orange",
     },
     {
-      question: '\nWhat are the colors of Rainbow ?\nTIP : Answer is just one word and has 7 letters.\n',
-      answer: 'vibgyor'
+      question:
+        "\nWhat are the colors of Rainbow ?\nTIP : Answer is just one word and has 7 letters.\n",
+      answer: "vibgyor",
     },
     {
-      question: '\nWhat would you call an image that has one color ?\n',
-      answer: 'monochrome'
+      question: "\nWhat would you call an image that has one color ?\n",
+      answer: "monochrome",
     },
     {
-      question: '\nWhat is the color of the stripes on a newly born zebra?\n',
-      answer: 'brown'
-    }
+      question: "\nWhat is the color of the stripes on a newly born zebra?\n",
+      answer: "brown",
+    },
   ];
 
   if (score === 10) {
-    console.log("\nInteresting... Let me ask you a few more...\n" + chalk.black.bgCyan("\nLevel Three"));
+    console.log(
+      "\nInteresting... Let me ask you a few more...\n" +
+        chalk.black.bgCyan("\nLevel Three")
+    );
 
     for (let i = 0; i < levelThreeQuestions.length; i++) {
       let currentQuestion = levelThreeQuestions[i];
-      ask(currentQuestion.question, currentQuestion.answer)
+      ask(currentQuestion.question, currentQuestion.answer);
     }
   }
 
   if (score === 15) {
-    console.log(chalk.black.bgWhite("\nCongratulations on answering all the questions right !\n"));
+    console.log(
+      chalk.black.bgWhite(
+        "\nCongratulations on answering all the questions right !\n"
+      )
+    );
   }
 
-  console.log(chalk.bgRed("\n×-×-×-×-×-×-×-×||×-×-×-×-×-×-×-×") + chalk.bgBlue("\nThankyou for playing."));
-  console.log(chalk.bold(("\nYour final score is ") + score));
+  console.log(
+    chalk.bgRed("\n×-×-×-×-×-×-×-×||×-×-×-×-×-×-×-×") +
+      chalk.bgBlue("\nThankyou for playing.")
+  );
+  console.log(chalk.bold("\nYour final score is " + score));
 
-	/*You can make the below 'if' condition more concise to 
+  /*You can make the below 'if' condition more concise to 
 	> if (score && score <= 2) 
 	as well because score > 0 is truthy*/
 
   if (score > 0 && score <= 5) {
     console.log("\nYou should work on your Knowledge about colors 😐 \n");
   } else if (score >= 6 && score <= 10) {
-    console.log("\nYou know quite well about Colors, but you still didn't answer all 15 question 😆 ");
+    console.log(
+      "\nYou know quite well about Colors, but you still didn't answer all 15 question 😆 "
+    );
   } else if (score >= 11 && score <= 15) {
-    console.log("Wow, seems like quiz on Colors is your forte !")
+    console.log("Wow, seems like quiz on Colors is your forte !");
   } else {
-    console.log("\nBut, seriously ? Negative score ? 😐\nYou better start googling about colors !");
+    console.log(
+      "\nBut, seriously ? Negative score ? 😐\nYou better start googling about colors !"
+    );
   }
 } else {
-  console.log("\nI didn't know you were weak in solving simple math question. Try again !!!");
+  console.log(
+    "\nI didn't know you were weak in solving simple math question. Try again !!!"
+  );
 }
